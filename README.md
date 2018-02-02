a0cc9d64de0a6157f6bc69a5d8e0ce8ebd34c97f
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-02 15:21:30 +0000

    Use gunicorn

---
+++ README.md
@@ -0,0 +1,1 @@
+Run with pipenv run gunicorn -w 4 -b 0.0.0.0:5000 run:app
--- run.py
+++ run.py
@@ -26,5 +26,5 @@ def load_user(email):
 def unauthorized_callback():
     return redirect('/')
 
-
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
