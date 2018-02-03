563df380e558e3940492098ef43ddf9695ff61f1
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-03 08:37:50 +0000

    Delete generate accounts

--- generate_account.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-from app.database import init_db, db_session
-from app.models import User
-
-init_db()
-user = User('benjefferies@echosoft.uk', bcrypt.hashpw('password'.encode(), bcrypt.gensalt()))
-db_session.add(user)
-db_session.commit()
