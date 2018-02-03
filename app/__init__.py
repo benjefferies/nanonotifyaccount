8498370d6b9e02aadfe871f67476f4fccfcd2f6a
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-03 13:53:12 +0000

    Improve logging format

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 import logging
 
-import sys
-
-logging.basicConfig()
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-root.addHandler(ch)
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
